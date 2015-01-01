b292fdb377d6b303015a26286b76e98b8fe154c1
mare hunt <mare.hunt@netgroup.ee>
2015-01-01 19:55:50 +0200

    testing materializecss

---
+++ Build/bower_components/materialize-master/Gruntfile.js
@@ -0,0 +1,343 @@
+module.exports = function(grunt) {
+ 
+  // configure the tasks
+  grunt.initConfig({
+//  Copy
+    copy: {
+      dist: { cwd: 'font', src: [ '**' ], dest: 'dist/font', expand: true },
+    },
+    
+//  Sass
+    sass: {                              // Task
+      expanded: {                            // Target
+        options: {                       // Target options
+          outputStyle: 'expanded',
+          sourcemap: false,
+        },
+        files: {                         
+          'dist/css/materialize.css': 'sass/materialize.scss',
+        }
+      },
+      
+      min: {
+        options: {
+          outputStyle: 'compressed',
+          sourcemap: false
+        },
+        files: {                         
+          'dist/css/materialize.min.css': 'sass/materialize.scss',
+        }
+      },
+      
+      // Compile ghpages css
+      gh: {
+        options: {
+          style: 'compressed',
+          sourcemap: false
+        },
+        files: {
+          'css/ghpages-materialize.css': 'sass/ghpages-materialize.scss',
+        }
+      },
+      
+      // Compile ghpages css
+      bin: {
+        options: {
+          style: 'expanded',
+          sourcemap: false
+        },
+        files: {
+          'bin/materialize.css': 'sass/materialize.scss',
+        }
+      }
+    },
+    
+//  Concat
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        // the files to concatenate
+        src: ["js/jquery.easing.1.3.js",
+             "js/velocity.min.js",
+              "js/hammer.min.js",
+              "js/jquery.hammer.js",
+              "js/collapsible.js",
+              "js/dropdown.js",
+              "js/leanModal.js",
+              "js/materialbox.js",
+              "js/parallax.js",
+              "js/tabs.js",
+              "js/tooltip.js",
+              "js/waves.js",
+              "js/toasts.js",
+              "js/sideNav.js",
+              "js/scrollspy.js",
+              "js/forms.js",
+              "js/slider.js",
+              "js/cards.js",
+              "js/date_picker/picker.js",
+              "js/date_picker/picker.date.js",
+             ],
+        // the location of the resulting JS file
+        dest: 'dist/js/materialize.js'
+      }
+    },
+
+//  Uglify
+    uglify: {
+      options: {
+              
+        // the banner is inserted at the top of the output
+//        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/js/materialize.min.js': ['dist/js/materialize.js']
+        }
+      },
+      bin: {
+        files: {
+          'bin/materialize.js': ['dist/js/materialize.js']
+        }
+      }
+    },
+
+
+//  Compress
+    compress: {
+      main: {
+        options: {
+          archive: 'bin/materialize.zip',
+          level: 6
+        },
+        files:[
+          { expand: true, cwd: 'dist/', src: ['**/*'], dest: 'materialize/'},
+          { expand: true, cwd: './', src: ['LICENSE', 'README.md'], dest: 'materialize/'},
+        ]
+      },
+                   
+      src: {
+        options: {
+          archive: 'bin/materialize-src.zip',
+          level: 6
+        },
+        files:[
+          {expand: true, cwd: 'font/', src: ['**/*'], dest: 'materialize-src/font/'},
+          {expand: true, cwd: 'sass/', src: ['materialize.scss'], dest: 'materialize-src/sass/'},
+          {expand: true, cwd: 'sass/', src: ['components/**/*'], dest: 'materialize-src/sass/'},
+          {expand: true, cwd: 'js/', src: ["jquery.easing.1.3.js",
+             "velocity.min.js",
+              "hammer.min.js",
+              "jquery.hammer.js",
+              "collapsible.js",
+              "dropdown.js",
+              "leanModal.js",
+              "materialbox.js",
+              "parallax.js",
+              "tabs.js",
+              "tooltip.js",
+              "waves.js",
+              "toasts.js",
+              "sideNav.js",
+              "scrollspy.js",
+              "forms.js",
+              "slider.js",
+              "cards.js",
+              "date_picker/picker.js",
+              "date_picker/picker.date.js",
+             ], dest: 'materialize-src/js/'},
+        {expand: true, cwd: 'dist/js/', src: ['**/*'], dest: 'materialize-src/js/bin/'},
+        {expand: true, cwd: './', src: ['LICENSE', 'README.md'], dest: 'materialize-src/'}
+        
+        ]
+      }
+    },
+                   
+    
+//  Clean
+//    clean: {
+//      build: {
+//        src: [ 'dist/' ]
+//      },
+//    },
+
+//  Jade
+    jade: {
+      compile: {
+        options: {
+          pretty: true,
+          data: {
+            debug: false
+          }
+        },
+        files: {
+          "index.html": "jade/index.jade",
+          "about.html": "jade/about.jade",
+          "getting-started.html": "jade/getting-started.jade",
+          "sass.html": "jade/sass.jade",
+          "components.html": "jade/components.jade",
+          "javascript.html": "jade/javascript.jade",
+          "mobile.html": "jade/mobile.jade",
+          "showcase.html": "jade/showcase.jade",
+          "parallax.html": "jade/parallax.jade"
+
+        }
+      }
+    },               
+                   
+//  Watch Files
+    watch: {
+      jade: {
+        files: ['jade/**/*'],
+        tasks: ['jade_compile'],
+        options: {
+          interrupt: false,
+          spawn: false,
+        },
+      },
+                   
+      js: {
+        files: [ "js/jquery.easing.1.3.js",
+                 "js/velocity.min.js",
+                 "js/hammer.min.js",
+                 "js/jquery.hammer.js",
+                 "js/collapsible.js",
+                 "js/dropdown.js",
+                 "js/leanModal.js",
+                 "js/materialbox.js",
+                 "js/parallax.js",
+                 "js/tabs.js",
+                 "js/tooltip.js",
+                 "js/waves.js",
+                 "js/toasts.js",
+                 "js/sideNav.js",
+                 "js/scrollspy.js",
+                 "js/forms.js",
+                 "js/slider.js",
+                 "js/cards.js",
+                 "js/date_picker/picker.js",
+                 "js/date_picker/picker.date.js",
+             ],
+        tasks: ['js_compile'],
+        options: {
+          interrupt: false,
+          spawn: false,
+        },
+      },
+      
+      sass: {
+        files: ['sass/**/*'],
+        tasks: ['sass_compile'],
+        options: {
+          interrupt: false,
+          spawn: false,
+        },           
+      }
+    },
+                   
+                   
+//  Concurrent
+    concurrent: {
+      options: {
+        logConcurrentOutput: true
+      },
+      monitor: {
+        tasks: ["watch:jade", "watch:js", "watch:sass", "notify:watching", 'connect:server', 'notify:server']
+      },
+    },
+                   
+                
+//  Notifications
+    notify: {
+      watching: {
+        options: {
+          enabled: true,
+          message: 'Watching Files!',
+          title: "Materialize", // defaults to the name in package.json, or will use project directory's name
+          success: true, // whether successful grunt executions should be notified automatically
+          duration: 1 // the duration of notification in seconds, for `notify-send only
+        }
+      },
+                   
+      sass_compile: {
+        options: {
+          enabled: true,
+          message: 'Sass Compiled!',
+          title: "Materialize",
+          success: true,
+          duration: 1
+        }
+      },
+                   
+      js_compile: {
+        options: {
+          enabled: true,
+          message: 'JS Compiled!',
+          title: "Materialize",
+          success: true,
+          duration: 1
+        }
+      },
+                   
+      jade_compile: {
+        options: {
+          enabled: true,
+          message: 'Jade Compiled!',
+          title: "Materialize",
+          success: true,
+          duration: 1
+        }
+      },       
+      
+      server: {
+        options: {
+          enabled: true,
+          message: 'Server Running!',
+          title: "Materialize",
+          success: true,
+          duration: 1
+        }
+      }
+    },
+                   
+              
+//  Server
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          useAvailablePort: true,
+          hostname: '*',
+          keepalive: true
+        }
+      }
+    }
+
+  });
+ 
+  // load the tasks
+//  grunt.loadNpmTasks('grunt-gitinfo');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+//  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // define the tasks
+  grunt.registerTask('default', ['copy', 'sass:expanded', 'sass:min', 'concat', 'uglify:dist', 'compress:main', 'compress:src']);
+  
+  grunt.registerTask('jade_compile', ['jade', 'notify:jade_compile']);
+  grunt.registerTask('js_compile', ['concat:dist', 'uglify:bin', 'notify:js_compile']);
+  grunt.registerTask('sass_compile', ['sass:gh', 'sass:bin', 'notify:sass_compile']);
+  grunt.registerTask('start_server', ['connect:server', 'notify:server']);
+  
+  grunt.registerTask("monitor", ["concurrent:monitor"]);
+};
--- README.md
+++ README.md
@@ -21,17 +21,24 @@ As an international project with participans from different locations/companies 
 Hanno has written about their tools for [collaboration](http://hanno.co/logbook/nine-tools-to-keep-our-remote-team-together), peek-in into their general workflow is [here](http://playbook.hanno.co/).
 Read about [Slack and Asana](http://qcmny.com/tools-and-technology/3-collaboration-tools-cant-live-without/)
 
-## Fonts
+## Fonts and directions
 We go with the bulletproofed [Google fonts](http://www.google.com/design/spec/style/typography.html#): Roboto (light, Normal, Medium e.g. 300, 400, 500) for english version, Noto for arabic and kurdish. Thats for the webpage. All the documents influenced by MS Word and possibly sent by email shall use unicode fonts.
 
 Other suggestionst for [arabic webfonts](http://stackoverflow.com/questions/7185106/what-are-my-web-font-choices-for-arabic) by community, in Google fonts are arabic-persian fonts under [early access fonts](http://www.google.com/fonts/earlyaccess). Note that arabic and kurdish letters are not the same.
 
 Pay attention that rtl-writing starts from end - in our understanding. They take the file and start to fill it from end. 
 
-## List of used plugins
+As we are going with SASS version, there is a helper for [rtl-support](http://www.matanich.com/2013/09/06/rtl-css-with-sass/)
+
+## Colors
+The color pallette should be nordic and unobtrusive - was the clients wish. The sample pallettes are in [Coolor account](http://coolors.co/092327-0b5351-00a9a5-b5c2b7-8c93a8).
+
+## List of used plugins and methods
 
 - frontpage and first user manual page scroll based on [pagePiling.js](https://github.com/alvarotrigo/pagePiling.js#pagepilingjs)
 - bubbles for guide tips - [crumble](http://blog.tommoor.com/crumble/)
+- general animation of elements on pagescroll - [wow.js](http://mynameismatthieu.com/WOW/). A decent [example](https://www.box.com/notes/) about it is Boxnotes.
+- [HTML5 fullscreen API](http://davidwalsh.name/fullscreen) Explained in detail [here](http://www.hongkiat.com/blog/html5-fullscreen-api/)
 
 ## Maybe-plugins
 - svg interaction to replace flash [snap.svg](http://snapsvg.io/)
@@ -42,7 +49,7 @@ Pay attention that rtl-writing starts from end - in our understanding. They take
 - inputs will look like [this](http://codepen.io/sevilayha/full/IdGKH/)
 -  the style is used in [Paper Elements](http://www.polymer-project.org/components/paper-elements/demo.html#paper-shadow)
 - Polymer tutorial by [Sitepoint](http://www.sitepoint.com/introduction-to-web-components-and-polymer-tutorial/)
--  sample admin themes to get started: [Bootbundle app](http://bootbundle.herokuapp.com/resources), [bootstraptor](http://demo.bootstraptor.com/bs_deal.html) (less-based) and [Detail Admin](http://wrapbootstrap.com/preview/WB07061TJ)
+-  sample admin themes to get started: [Bootbundle app](http://bootbundle.herokuapp.com/resources), [bootstraptor](http://demo.bootstraptor.com/bs_deal.html) (less-based) and [Detail Admin](http://wrapbootstrap.com/preview/WB07061TJ). And cool angular-template [Flafull](http://flatfull.com/themes/angulr/#/app/docs) - pay attention to ui-jq what allows you to call the jQuery function or plugin specified on the element.
 
 
 ## Usability testing
