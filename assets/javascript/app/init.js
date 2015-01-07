6bfe91594a80cb95aa2a9bdd478404b0b326e2fa
mare hunt <mare.hunt@netgroup.ee>
2015-01-08 01:00:25 +0200

    prime regitry entry progress

--- assets/javascript/app/init.js
+++ assets/javascript/app/init.js
@@ -8,5 +8,12 @@
     });
     $('.collapsible').collapsible();
     $(".button-collapse").sideNav();
+    $('.datepicker').pickadate({
+       onStart: function ()
+            {
+                var date = new Date();
+                this.set('select', [date.getFullYear(), date.getMonth() + 1, date.getDate()]);
+            }
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
