7a7509136bd68a5aefa5ab29eb53c45ac3d330c2
mare hunt <mare.hunt@netgroup.ee>
2015-01-10 20:02:15 +0200

    frontpage update w pagepiling

--- Build/assets/javascript/app/init.js
+++ Build/assets/javascript/app/init.js
@@ -8,12 +8,31 @@
     });
     $('.collapsible').collapsible();
     $(".button-collapse").sideNav();
+
+    // pickadate init with preset Today
     $('.datepicker').pickadate({
        onStart: function ()
-            {
-                var date = new Date();
-                this.set('select', [date.getFullYear(), date.getMonth() + 1, date.getDate()]);
-            }
+        {
+          var date = new Date();
+          this.set('select', [date.getFullYear(), date.getMonth() + 1, date.getDate()]);
+        }
     });
+    // wow.js init
+    new WOW().init();
+
+    // init  dataTables
+    // $('table').dataTable({
+    //   "scrollX": true
+    // });
+
+    $('.home').pagepiling({
+        touchSensitivity: 5,
+        sectionSelector: '.pagepiling',
+        verticalCentered: true,
+        menu: '#menu',
+        anchors: ['Start', 'page2', 'page3'],
+        navigation: false
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
